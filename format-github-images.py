20b592fe8daf95d205f82b5b569c10ded1d2a609
Yifei Yin <yifeiyin@foxmail.com>
2020-05-01 01:25:26 -0400

    Create format-github-images.py

---
+++ format-github-images.py
@@ -0,0 +1,24 @@
+''' Use this bash/zsh script to make it work with pbcopy and pbpaste.
+
+# !zsh
+input=$(pbpaste)
+urls=$(grep -Eoi '(http|https)://[^\)]+' <<< "$input")
+python3 url-process.py <<< "$urls" | pbcopy
+'''
+
+import sys
+
+a = open(0).read()
+
+if len(a) < 20 or len(a) > 500:
+    print("Content size is " + len(a));
+    exit(1);
+
+print('<p>')
+for line in a.split():
+    print('<img float="left" width="32%" src="' + line + '" />')
+
+print('</p>')
+print('<!--')
+print(a, end='')
+print('-->')
